6ab935f15a6e30d1d052e767824da50a466bb1ed
Gasrprinskiy <gasprin321@gmail.com>
2023-06-06 12:03:43 +0500

    feat(entity product): new params in FindProductListParam

--- jrpc-server/src/internal/entity/product/params/index.ts
+++ jrpc-server/src/internal/entity/product/params/index.ts
@@ -9,4 +9,8 @@ export interface FindProductListParam {
     limit: number;
     offset: number;
     employee_login: string;
+    query: string | null;
+    price_from: number | null;
+    price_till: number | null;
+    show_all: boolean;
 }
