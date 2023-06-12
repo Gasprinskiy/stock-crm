5a4b5a32628bc5721779e2e3c5e55c28ab0ed669
Gasrprinskiy <gasprin321@gmail.com>
2023-06-12 14:13:09 +0500

    fix(repo product): edit product search sql query

--- jrpc-server/src/internal/repository/postgres/product.ts
+++ jrpc-server/src/internal/repository/postgres/product.ts
@@ -2,12 +2,13 @@ import pgPromise from "pg-promise";
 import { Product } from "../../entity/product/entity/index.js";
 import { CreateProductParam, FindProductListParam, ProductPriceRange } from "../../entity/product/params/index.js"
 import { handleRequestError } from "../../../tools/pgerrhandler/index.js";
+import { CountResponse } from "../../entity/global/entity/index.js";
 
 export interface ProductRepoInter {
     GetProductByID(ts: pgPromise.ITask<{}>, id: number): Promise<Product|Error>;
     CreateProduct(ts: pgPromise.ITask<{}>, p: CreateProductParam): Promise<Product|Error>;
-    FindProductList(ts: pgPromise.ITask<{}>, p: FindProductListParam, stockID: number): Promise<Product[]|Error>;
-    FindProductCount(ts: pgPromise.ITask<{}>,  stockID: number): Promise<number|Error>;
+    FindProductListByStockID(ts: pgPromise.ITask<{}>, p: FindProductListParam, stockID: number): Promise<Product[]|Error>;
+    FindProductCount(ts: pgPromise.ITask<{}>,  p: FindProductListParam, stockID: number): Promise<CountResponse|Error>;
     // LoadPriceRange(ts: pgPromise.ITask<{}>): Promise<ProductPriceRange|Error>
 }
 
@@ -33,46 +34,30 @@ export class ProductRepository implements ProductRepoInter {
         return ts.one(sqlQuery)
     }
 
-    public async FindProductList(ts: pgPromise.ITask<{}>, p: FindProductListParam, stockID: number): Promise<Product[]|Error> {
+    public async FindProductListByStockID(ts: pgPromise.ITask<{}>, p: FindProductListParam, stockID: number): Promise<Product[]|Error> {
         const sqlQuery = `
         SELECT 
             p.product_id, 
             p.product_name, 
             p.description, 
-            p.tags, 
-            p.creation_date, 
-            ps.amount, 
-            ps.stock_id, 
-            pr.price
+            p.tags,
+            CAST(SUM(ps.amount) as INTEGER) as total_amount
         FROM product p
             JOIN product$stocks ps on(ps.product_id = p.product_id)
-            LEFT OUTER JOIN product$price pr on(pr.variation_id = ps.variation_id)
-        WHERE ps.stock_id = COALESCE(${stockID}, ps.stock_id)
-        ${p.price_range ? `AND (
-            pr.price BETWEEN ${p.price_range.max_price} and ${p.price_range.max_price}
-        )` : ""}
-        ${p.query ? `AND (
-            LOWER(p.product_name) like LOWER('%${p.query}%')
-            OR
-            LOWER(p.tags) like LOWER('%${p.query}%')
-        )`: ""}
-        ${p.show_all ? "" : "AND ps.amount > 0"}
-        ORDER BY p.product_id
-        DESC
-        LIMIT ${p.limit}
-        OFFSET ${p.offset}`
+        ${this.findProductListFilterQuery(p, stockID)}
+        ${this.findProductListGroupQuery(p)}`
 
         return handleRequestError(() => {
             return ts.many(sqlQuery)
         })
     }
 
-    public async FindProductCount(ts: pgPromise.ITask<{}>, stockID: number): Promise<number|Error> {
+    public async FindProductCount(ts: pgPromise.ITask<{}>, p: FindProductListParam, stockID: number): Promise<CountResponse|Error> {
         const sqlQuery = `
-        SELECT count(pr.product_id)
-        FROM product pr
-            JOIN product$stocks ps ON(ps.product_id = pr.product_id)
-        ${stockID ? `WHERE ps.stock_id = ${stockID}`: ""}`
+        SELECT count(DISTINCT p.product_id)
+        FROM product p
+            JOIN product$stocks ps ON(ps.product_id = p.product_id)
+        ${this.findProductListFilterQuery(p, stockID)}`
 
         return handleRequestError(() => {
             return ts.one(sqlQuery, stockID)
@@ -85,4 +70,27 @@ export class ProductRepository implements ProductRepoInter {
     //         max_price: 10000.0
     //     }
     // }
+
+    private findProductListFilterQuery(p: FindProductListParam, stockID: number): string {
+        return `
+        WHERE ps.stock_id = COALESCE(${stockID}, ps.stock_id)
+        ${p.price_range ? `AND (
+            pr.price BETWEEN ${p.price_range.min_price} and ${p.price_range.max_price}
+        )` : ""}
+        ${p.query ? `AND (
+            LOWER(p.product_name) like LOWER('%${p.query}%')
+            OR
+            LOWER(p.tags) like LOWER('%${p.query}%')
+        )`: ""}
+        ${p.show_all ? "" : "AND ps.amount > 0"}`
+    }
+
+    private findProductListGroupQuery(p: FindProductListParam): string {
+        return `
+        GROUP BY p.product_id
+        ORDER BY p.product_id
+        DESC
+        LIMIT ${p.limit}
+        OFFSET ${p.offset}`
+    }
 }
