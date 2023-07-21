9fc4da272334bbe62a0f666b6e54781abceafa11
Gasrprinskiy <gasprin321@gmail.com>
2023-07-21 14:55:15 +0500

    real(repo-import): init stock repository

--- jrpc-server/src/internal/repository/index.ts
+++ jrpc-server/src/internal/repository/index.ts
@@ -1,11 +1,14 @@
 import { EmployeeRepoInter, EmployeeRepo } from "./postgres/employee.js";
 import { ProductRepoInter, ProductRepository } from "./postgres/product.js";
+import { StockRepo } from "./postgres/stock.js";
 
 export class Repository {
     Product: ProductRepoInter;
     Employee: EmployeeRepoInter;
+    Stock: StockRepo;
     constructor(){
         this.Product = new ProductRepository();
         this.Employee = new EmployeeRepo()
+        this.Stock = new StockRepo()
     }
 }
