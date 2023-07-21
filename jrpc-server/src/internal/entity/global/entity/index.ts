587c4998cf77a0ecf67101f08f383e3d96ef5a53
Gasrprinskiy <gasprin321@gmail.com>
2023-07-21 15:00:07 +0500

    real(entity-global): express Request type redeclare with custom key 'user'

--- jrpc-server/src/internal/entity/global/entity/index.ts
+++ jrpc-server/src/internal/entity/global/entity/index.ts
@@ -1,3 +1,13 @@
+import { ApiUser } from "../../rest/entity/index.js";
+
 export interface CountResponse {
     count: number;
+}
+
+declare global {
+    namespace Express {
+      export interface Request {
+        user: ApiUser;
+      }
+    }
 }
