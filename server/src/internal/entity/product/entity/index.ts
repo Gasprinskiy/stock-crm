99d01e6668d85687065617b914fc0d9186d48301
Gasprinskiy <gasprin321@gmail.com>
2023-07-23 21:32:14 +0500

    real)entity-product): ProductVariation

--- server/src/internal/entity/product/entity/index.ts
+++ server/src/internal/entity/product/entity/index.ts
@@ -10,3 +10,9 @@ export interface ProductListResponse {
     product_list: Product[];
     page_count: number;
 }
+
+export interface ProductVariation {
+    variation_id: number;
+    product_id: number;
+    v_type_id: number;
+}
