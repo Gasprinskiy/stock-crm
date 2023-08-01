958e80c61a4d1d46e958b8ba152720ad6b30908a
Gasrprinskiy <gasprin321@gmail.com>
2023-08-01 12:07:44 +0500

    fix(employee-repo-entity): employee does not have oper_id

--- server/src/internal/entity/employee/entity/index.ts
+++ server/src/internal/entity/employee/entity/index.ts
@@ -3,7 +3,6 @@ import { AccessRight } from "../constant/index.js";
 export interface Employee {
     empl_id: number;
     ar_id: AccessRight;
-    stock_id: number;
     fio: string;
     login: string;
     password: string;
--- server/src/internal/repository/postgres/employee.ts
+++ server/src/internal/repository/postgres/employee.ts
@@ -8,15 +8,16 @@ export interface EmployeeRepoInter {
 }
 
 export class EmployeeRepo implements EmployeeRepoInter {
-    public async CreateEmployee(ts: pg.PoolClient, p: Employee): Promise<EmployeeAuthResult> {
+    public CreateEmployee(ts: pg.PoolClient, p: Employee): Promise<EmployeeAuthResult> {
         const sqlQuery = `
-        INSERT INTO employees(ar_id, stock_id, fio, login, password)
-        VALUES (${p.ar_id}, ${p.stock_id}, '${p.fio}', '${p.login}', '${p.password}')
+        INSERT INTO employees(ar_id, fio, login, password)
+        VALUES (${p.ar_id}, '${p.fio}', '${p.login}', '${p.password}')
         RETURNING empl_id, ar_id, login`
 
         return insert(ts, sqlQuery, true)
     }
-    public async GetEmployeeByLogin(ts: pg.PoolClient, login: string): Promise<Employee> {
+
+    public GetEmployeeByLogin(ts: pg.PoolClient, login: string): Promise<Employee> {
         const sqlQuery = `
         SELECT e.empl_id, e.ar_id, e.fio, e.login, e.password
         FROM employees e
