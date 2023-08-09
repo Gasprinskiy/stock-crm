6695d75b0b68200a40a3181efed55c2f10f9d82b
Gasprinskiy <gasprin321@gmail.com>
2023-08-09 20:56:34 +0500

    feat(statistics-repo)

--- server/src/internal/repository/index.ts
+++ server/src/internal/repository/index.ts
@@ -3,12 +3,14 @@ import { EmployeeRepository, EmployeeRepositoryImpl } from "./postgres/employee.
 import { ProductRepository, ProductRepositoryImpl } from "./postgres/product.js";
 import { StockRepositoryImplsitory, StockRepositoryImpl } from "./postgres/stock.js";
 import { EmployeeCache, EmployeeCacheImpl } from "./redis/employee_chache.js";
+import { StatisticsRepository, StatisticsRepositoryImpl } from './postgres/statistics.js';
 
 export class Repository {
     Product: ProductRepository;
     Employee: EmployeeRepository;
     EmployeeCache: EmployeeCache;
     Stock: StockRepositoryImplsitory;
+    Statistics: StatisticsRepository;
     constructor(params:{
         redis: RedisClientType
     }){
@@ -16,5 +18,6 @@ export class Repository {
         this.Employee = new EmployeeRepositoryImpl()
         this.Stock = new StockRepositoryImpl()
         this.EmployeeCache = new EmployeeCacheImpl(params.redis)
+        this.Statistics = new StatisticsRepositoryImpl()
     }
 }
--- server/src/internal/repository/postgres/statistics.ts
+++ server/src/internal/repository/postgres/statistics.ts
@@ -1,4 +1,23 @@
+import pg from 'pg';
+import { CommonStatistics } from "../../entity/statistics/entity/index.js";
+import { select } from '../../../tools/repository-generic/index.js';
 
-// export interface StatisticsRepo {
-//     LoadCommonStatistics(ts: pg.PoolClient);
-// }
+export interface StatisticsRepository {
+    LoadCommonStatistics(ts: pg.PoolClient) : Promise<CommonStatistics[]>;
+}
+
+export class StatisticsRepositoryImpl implements StatisticsRepository {
+    LoadCommonStatistics(ts: pg.PoolClient) :  Promise<CommonStatistics[]> {
+        const sqlQuery = `
+        SELECT 
+            SUM(pp.price) as sales_sum, 
+            SUM(psa.amount) as sales_amount,
+            (SELECT SUM(prs.amount) FROM product$stocks prs) as product_amount,
+            (SELECT COUNT(s.stock_id) FROM stocks s) as stock_amount,
+            (SELECT COUNT(em.empl_id) FROM employees em) as employee_count
+        FROM product$sales psa
+          JOIN product$price pp ON(pp.variation_id = psa.variation_id)
+        `  
+        return select(ts, sqlQuery)
+    }
+}
