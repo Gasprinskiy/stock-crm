7213866d5a243421c5c62742caccd7d698129d77
Gasprinskiy <gasprin321@gmail.com>
2023-08-17 21:22:59 +0500

    feat(server-entity-stock): StockListResponse with page count field

--- server/src/internal/entity/stock/entity/entity.ts
+++ server/src/internal/entity/stock/entity/entity.ts
@@ -3,4 +3,10 @@ export interface Stock {
     stock_id: number;
     name: string;
     address: string;
+    product_count: number;
+}
+
+export interface StockListResponse {
+    stock_list: Stock[];
+    page_count: number;
 }
