546f3b9e3defedd750fdf359870ba5a8ea343bfb
Gasprinskiy <gasprin321@gmail.com>
2023-08-17 21:29:20 +0500

    style(client-api-request-composable): method does returns callback with args;

--- client/src/composables/api_request/index.ts
+++ client/src/composables/api_request/index.ts
@@ -1,12 +1,11 @@
 import { useNotification } from "naive-ui";
 
-export const useApiRequestHandler = <T>(callback: (args?: any) => Promise<T>, args?: any):  () => Promise<T | null> => {
+export const useApiRequestHandler = <T>(callback: (args?: any) => Promise<T>):  (args?: any) => Promise<T | null> => {
     const notification = useNotification()
 
-    return async function() : Promise<T | null> {
+    return async function(args?: any) : Promise<T | null> {
         try {        
-            const response = await callback(args)
-            return response
+            return await callback(args)
         } catch(err: any) {
             notification.error({
                 content: err,
