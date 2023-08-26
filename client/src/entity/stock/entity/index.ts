5af3dafb8a9d410badbf5107362cf0bd325529fe
Gasprinskiy <gasprin321@gmail.com>
2023-08-26 19:18:51 +0500

    style(client-entity-stock): StockProductStats, StockSaleStats entities from common entity stock

--- client/src/entity/stock/entity/index.ts
+++ client/src/entity/stock/entity/index.ts
@@ -1,9 +1,18 @@
+
 export interface Stock {
     stock_id: number;
     name: string;
     address: string;
+}
+
+export interface StockProductStats extends Stock {
     product_count: number;
     movement_in_count: number;
+}
+
+export interface StockSaleStats extends Stock {
+    today_sales: number;
+    total_sales: number;
 }
 
 export interface StockListResponse {
