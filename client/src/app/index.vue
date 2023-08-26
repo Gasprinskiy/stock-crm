0d29747a9b98294217647dc06907788138ec53bd
Gasprinskiy <gasprin321@gmail.com>
2023-08-26 19:22:32 +0500

    feat(cleint-app-wrapper): naive ui dialog provider

--- client/src/app/index.vue
+++ client/src/app/index.vue
@@ -1,12 +1,14 @@
 <template>
   <n-loading-bar-provider>
     <n-notification-provider>
-      <app/>
+      <n-dialog-provider>
+        <app/>
+      </n-dialog-provider>
     </n-notification-provider>
   </n-loading-bar-provider>
 </template>
 
 <script setup lang="ts">
-import { NNotificationProvider, NLoadingBarProvider } from 'naive-ui'
+import { NNotificationProvider, NLoadingBarProvider, NDialogProvider } from 'naive-ui'
 import app from '@/app/app.vue'
 </script>
