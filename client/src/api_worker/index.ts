eb93372de16ec553cb308194aa2f80a06577310d
Gasprinskiy <gasprin321@gmail.com>
2023-08-26 19:04:46 +0500

    feat(client-api-worker): new core injection

--- client/src/api_worker/index.ts
+++ client/src/api_worker/index.ts
@@ -1,23 +1,36 @@
-import { StatisticsApiWorker, StatisticsApiWorkerImpl } from './core/statistics';
 import { InjectionKey } from 'vue'
 import { EmployeeApiWorker, EmployeeApiWorkerImpl } from './core/employee'
+import { StatisticsApiWorker, StatisticsApiWorkerImpl } from './core/statistics';
 import { StockApiWorker, StockApiWorkerImpl } from './core/stock'
+import { ProductApiWorker, ProductApiWorkerImpl } from './core/product';
+import { SalesApiWorker, SalesApiWorkerImpl } from './core/sales';
 
 type InjectionKeys = {[key: string]: InjectionKey<any>}
 type InjectionImpl = {[key: string]: object}
 
+export const ProductApiWorkerInjectionKey = Symbol() as InjectionKey<ProductApiWorker>
 export const EmployeeApiWorkerInjectionKey = Symbol() as InjectionKey<EmployeeApiWorker>
 export const StockApiWorkerInjectionKey = Symbol() as InjectionKey<StockApiWorker>
 export const StatisticsApiWorkerInjectionKey = Symbol() as InjectionKey<StatisticsApiWorker>
+export const SalesApiWorkerInjectionKey = Symbol() as InjectionKey<SalesApiWorker>
 
-export const InjectionKeysMap: InjectionKeys = {
+const InjectionKeysMap: InjectionKeys = {
     employee: EmployeeApiWorkerInjectionKey,
     stock: StockApiWorkerInjectionKey,
-    statistics: StatisticsApiWorkerInjectionKey
+    statistics: StatisticsApiWorkerInjectionKey,
+    product: ProductApiWorkerInjectionKey,
+    sales: SalesApiWorkerInjectionKey
 }
 
-export const InjectionImplMap : InjectionImpl = {
+const InjectionImplMap : InjectionImpl = {
     employee: new EmployeeApiWorkerImpl(),
     stock: new StockApiWorkerImpl(),
-    statistics: new StatisticsApiWorkerImpl()
+    statistics: new StatisticsApiWorkerImpl(),
+    product: new ProductApiWorkerImpl(),
+    sales: new SalesApiWorkerImpl()
+}
+
+export default {
+    injection_keys: InjectionKeysMap,
+    injection_impl: InjectionImplMap
 }
