cf6bc32634027317648e23dd086b8e406bc3c219
Gasprinskiy <gasprin321@gmail.com>
2023-08-26 19:02:07 +0500

    fix(client-api-worker): using localStorage instead of coockie bcs of doc. coockie bug

--- client/src/api_worker/axios/index.ts
+++ client/src/api_worker/axios/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import appBus from "@/shared/app-bus"
+import appBus from "@/shared/app_bus"
 
 const instance = axios.create({
     baseURL: 'http://127.0.0.3:3000',
@@ -12,9 +12,10 @@ const instance = axios.create({
 instance.interceptors.request
 .use(
     (request) => {
-        appBus.emit('api-request-started')    
-        if (document.cookie) {
-            request.headers.Authorization = document.cookie
+        appBus.emit('api-request-started')
+        const token = localStorage.getItem("token")
+        if (token) {
+            request.headers.Authorization = token
         }
         return request
     }
@@ -25,7 +26,7 @@ instance.interceptors.response
     (response) => {
         appBus.emit('api-request-finished-successful')
         if (response.headers.authorization) {
-            document.cookie = response.headers.authorization
+            localStorage.setItem("token", response.headers.authorization)
         }
         return response.data
     },
