8a0e71aeec8b3bf3f6f3747176493d877e6b4f7d
Gasprinskiy <gasprin321@gmail.com>
2023-08-26 19:04:09 +0500

    feat(client-sales-api-worker): sales stat stock list load

--- client/src/api_worker/core/stock.ts
+++ client/src/api_worker/core/stock.ts
@@ -1,13 +1,18 @@
-import { LoadParams } from './../../../../server/src/internal/entity/global/entity/index';
+import { LoadParams } from '@/entity/global/params/';
 import { StockListResponse } from "@/entity/stock/entity";
 import { handleApiGetRequest } from "../axios";
 
 export interface StockApiWorker {
-    findEmployeeStockList(loadParams: LoadParams): Promise<StockListResponse>;
+    findEmployeeProductStatStockList(loadParams: LoadParams): Promise<StockListResponse>;
+    findSalesStatStockListByEmployeeID(loadParams: LoadParams): Promise<StockListResponse>;
 }
 
 export class StockApiWorkerImpl implements StockApiWorker {
-    findEmployeeStockList(loadParams: LoadParams): Promise<StockListResponse> {
-        return handleApiGetRequest("find_empl_stocks", loadParams)
+    findEmployeeProductStatStockList(loadParams: LoadParams): Promise<StockListResponse> {
+        return handleApiGetRequest("empl_product_stat_stocks", loadParams)
+    }
+
+    findSalesStatStockListByEmployeeID(loadParams: LoadParams): Promise<StockListResponse> {
+        return handleApiGetRequest("empl_sales_stat_stocks", loadParams)
     }
 }
